68367091547b9d93bba48a18488fb2ee34be1d36
cherkaoui <soufiane.cherkaouiii@gmail.com>
2018-01-07 17:22:17 +0100

    test

---
+++ src/ErUserBundle/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ErUserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class AdminController extends Controller{
+    
+    /**
+     * @Route("/adminPage", name="admin_page")
+     */
+    public function adminPageAction(Request $request)
+    {
+        return $this->render("ErUserBundle:Admin:adminPage.html.twig");
+        $test =1;
+    }
+}
